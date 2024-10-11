a3cfe2b44e395e6127fb0bc9e126f709883b8a21
Leo <icefoxz@msn.com>
2024-10-11 21:53:40 +0800

    功能：闪电控件

---
+++ Light/Assets/_Scripts/Components/EnvironmentComponent.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using MyBox;
+using Sirenix.OdinInspector;
+using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
+
+/// <summary>
+/// 环境控件，主要控制闪雷
+/// </summary>
+public class EnvironmentComponent : CountdownComponent
+{
+    protected override int PulseTimes { get; } = 1;
+    protected override float Duration => _duration;
+    [SerializeField] Light2D globalLight; // 全局光照组件
+    [SerializeField, LabelText("闪电随机范围值")] MinMaxFloat _lightningRange;
+    [SerializeField,LabelText("闪电时的光强度")] float lightningIntensity = 0.15f;
+    [SerializeField,LabelText("闪电持续时间")] float lightningInterval = 0.15f;
+    float _duration = 3;
+
+    void Start() => Init();
+
+    void Init()
+    {
+        OnCountdownComplete.AddListener(Lightning);
+        RandomDuration();
+        StartCountdown();
+    }
+
+    void RandomDuration() => _duration = Random.Range(_lightningRange.Min, _lightningRange.Max);
+
+    void Lightning()
+    {
+        RandomDuration();
+        var times = Random.Range(2, 4);
+        StartCoroutine(LightningEffect(times, lightningInterval, StartCountdown));
+    }
+
+    IEnumerator LightningEffect(int times,float interval,UnityAction onLightningFinish)
+    {
+        // 保存原始光强度
+        float originalIntensity = globalLight.intensity;
+        var t = times;
+        while (t > 0)
+        {
+            yield return Lightning();
+            t--;
+        }
+        onLightningFinish?.Invoke();
+        yield break;
+
+        IEnumerator Lightning()
+        {
+            // 设置全局光为闪电的光强度
+            globalLight.intensity = lightningIntensity;
+            // 闪电持续时间
+            yield return new WaitForSeconds(interval);
+            // 恢复原始光强度
+            globalLight.intensity = originalIntensity;
+            yield return new WaitForSeconds(interval/3f);
+        }
+    }
+}
